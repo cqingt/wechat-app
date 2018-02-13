00bb6a1e956ade47d28c5e336c56c4d079f517c6
cqingt <784966200@qq.com>
2018-02-13 18:35:19 +0800

    评论列表

--- application/common/model/ProductComment.php
+++ application/common/model/ProductComment.php
@@ -11,23 +11,47 @@ class ProductComment extends Model
     // 开启自动写入时间戳字段
     protected $autoWriteTimestamp = 'int';
 
-    public function getTotal($productId)
+    /**
+     * @param $productId
+     * @param int $type
+     * @return int|string
+     */
+    public function getTotal($productId, $type = 0)
     {
-        return $this->where(['product_id' => $productId, 'enabled' => 'Y'])->count();
+        return $this->where(array_merge(['product_id' => $productId, 'enabled' => 'Y'], $this->getCondition($type)))->count();
     }
 
+    public function getCondition($type = 0)
+    {
+        if ($type > 0 && $type < 4) {
+            $condition = ['c.type' => $type];
+        } else if ($type == 4) {
+            $condition = ['c.images' => ['NEQ', '']];
+        } else {
+            $condition = [];
+        }
+        return $condition;
+    }
+
+    /**
+     * @param $productId
+     * @param int $type
+     * @param $offset
+     * @param int $rows
+     * @return array
+     */
     public function getComments($productId, $type = 0, $offset, $rows = 12)
     {
-        $comments = $this->alias('c')
+        $commentList = $this->alias('c')
             ->join('user u', 'u.id = c.user_id')
-            ->find(['u.username', 'u.avatar', 'c.content', 'c.images', 'c.create_time'])
-            ->where(['c.product_id' => $productId, 'c.enabled'=> 'Y'])
+            ->field(['u.username', 'u.avatar', 'c.content', 'c.images', 'c.create_time'])
+            ->where(array_merge(['c.product_id' => $productId, 'c.enabled'=> 'Y'], $this->getCondition($type)))
             ->order('c.id desc')
             ->limit($offset, $rows)
             ->select()
             ->toArray();
 
-        return $comments;
+        return $commentList;
     }
 
     // 最新一条评论
